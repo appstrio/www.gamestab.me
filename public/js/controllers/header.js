84d8882f4516f26d4df13a98046b5ac763e063ee
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2014-02-06 15:34:34 +0200

    moved mean to the current branch. will start applying gamestab to it

---
+++ public/js/controllers/header.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
+    $scope.global = Global;
+
+    $scope.menu = [{
+        'title': 'Menu',
+        'link': 'Menu'
+    }, {
+
+    }];
+    
+    $scope.isCollapsed = false;
+}]);
